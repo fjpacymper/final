63faf3d22603e606226f8edc87cf09bed4716f18
fjpacymper <fjpa@cymper.com>
2016-10-28 22:43:47 +0100

    Add files via upload

---
+++ fotos.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	/* Activación del slider */
+	$(document).ready(function() {
+	  $('.slider').slick({
+	    autoplay: true,
+	    autoplaySpeed: 2000,
+	    speed: 500,
+	    dots: true,
+	    arrows: true,
+	  });
+	});
+});
